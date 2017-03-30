a0f106920113b93a00754a3783d20ee9dc8deaf0
Yosmany Garcia <yosmanyga@gmail.com>
2017-03-29 23:22:31 -0700

    Fixed crontab filenames. Added output

--- docker/cron/entrypoint.sh
+++ docker/cron/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Note: Dont create files containing dot(.)
+
 cp -R /root/1min /etc/periodic/1min
 cp -R /root/1hour /etc/periodic/1hour
 
@@ -9,4 +11,8 @@ chmod +x /etc/periodic/1hour/*
 echo "* * * * * run-parts /etc/periodic/1min" | crontab -
 (crontab -l; echo "0 * * * * run-parts /etc/periodic/1hour") | crontab -
 
-exec crond -f
+touch /var/log/cron.log
+
+crond
+
+exec tail -f /var/log/cron.log
