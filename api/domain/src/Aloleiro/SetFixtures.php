11cf52a9f01cbeda1fb477f9dc4f69ecc5619fbc
Yosmany Garcia <yosmanyga@gmail.com>
2017-04-18 00:50:57 -0700

    Fixed service removing unused dependencies

--- api/domain/src/Aloleiro/SetFixtures.php
+++ api/domain/src/Aloleiro/SetFixtures.php
@@ -95,7 +95,6 @@ class SetFixtures
      */
     public function __construct(
         ManageBusinessStorage $manageBusinessStorage,
-        ManageUniquenessStorage $manageUniquenessStorage,
         ManageProfileStorage $manageProfileStorage,
         ManagePhoneStorage $managePhoneStorage,
         ManageCallStorage $manageCallStorage,
@@ -110,7 +109,6 @@ class SetFixtures
     )
     {
         $this->manageBusinessStorage = $manageBusinessStorage;
-        $this->manageUniquenessStorage = $manageUniquenessStorage;
         $this->manageProfileStorage = $manageProfileStorage;
         $this->managePhoneStorage = $managePhoneStorage;
         $this->manageCallStorage = $manageCallStorage;
