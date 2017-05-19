5ae32e24ba3de48893367a74cd8df126122f8ac5
Yosmany Garcia <yosmanyga@gmail.com>
2017-05-18 19:25:01 -0700

    Fixed some variable names

--- api/domain/vendor/cubalider-voip/src/Nexmo/PostProcessCalls.php
+++ api/domain/vendor/cubalider-voip/src/Nexmo/PostProcessCalls.php
@@ -56,7 +56,10 @@ class PostProcessCalls
                     && $event['status'] == 'completed'
                 ) {
                     // Less than 30 seconds?
-                    if (strtotime($event['end_time']) + 30 > time()) {
+                    if (
+                        isset($event['end_time'])
+                        && strtotime($event['end_time']) + 30 > time()
+                    ) {
                         // Ignore this call, let's wait more than 30 seconds,
                         // to make sure that nexmo sends all events
                         break;
@@ -65,29 +68,29 @@ class PostProcessCalls
                     $completed = true;
 
                     if ($event['direction'] == 'inbound') {
-                        $inbound['price'] = isset($inbound['end_time'])
-                            ? $inbound['price'] : null;
+                        $inbound['price'] = isset($event['end_time'])
+                            ? $event['price'] : null;
 
-                        $inbound['duration'] = isset($inbound['end_time'])
-                            ? $inbound['duration'] : null;
+                        $inbound['duration'] = isset($event['end_time'])
+                            ? $event['duration'] : null;
 
-                        $inbound['start_time'] = isset($inbound['end_time'])
-                            ? $inbound['start_time'] : null;
+                        $inbound['start_time'] = isset($event['end_time'])
+                            ? $event['start_time'] : null;
                         
-                        $inbound['end_time'] = isset($inbound['end_time'])
-                            ? $inbound['end_time'] : null;
+                        $inbound['end_time'] = isset($event['end_time'])
+                            ? $event['end_time'] : null;
                     } else {
-                        $outbound['price'] = isset($outbound['end_time'])
-                            ? $outbound['price'] : null;
+                        $outbound['price'] = isset($event['end_time'])
+                            ? $event['price'] : null;
 
-                        $outbound['duration'] = isset($outbound['end_time'])
-                            ? $outbound['duration'] : null;
+                        $outbound['duration'] = isset($event['end_time'])
+                            ? $event['duration'] : null;
 
-                        $outbound['start_time'] = isset($outbound['end_time'])
-                            ? $outbound['start_time'] : null;
+                        $outbound['start_time'] = isset($event['end_time'])
+                            ? $event['start_time'] : null;
 
-                        $outbound['end_time'] = isset($outbound['end_time'])
-                            ? $outbound['end_time'] : null;
+                        $outbound['end_time'] = isset($event['end_time'])
+                            ? $event['end_time'] : null;
                     }
                 }
             }
@@ -118,17 +121,17 @@ class PostProcessCalls
                 /* Start time, end time and duration from inbound are more
                    important than the outbound values. So let's override them */
 
-                $start = !is_null($outbound['start_time'])
-                    ? strtotime($outbound['start_time']) : null;
-                $end = !is_null($outbound['end_time'])
-                    ? strtotime($outbound['end_time']) : null;
-                $duration = !is_null($outbound['duration'])
-                    ? (int) $outbound['duration'] : null;
+                $start = !is_null($inbound['start_time'])
+                    ? strtotime($inbound['start_time']) : null;
+                $end = !is_null($inbound['end_time'])
+                    ? strtotime($inbound['end_time']) : null;
+                $duration = !is_null($inbound['duration'])
+                    ? (int) $inbound['duration'] : null;
 
                 /* Cost is added to the outbound */
 
-                $cost += $cost + !is_null($outbound['price'])
-                    ? (float) $outbound['price'] : 0;
+                $cost += $cost + !is_null($inbound['price'])
+                    ? (float) $inbound['price'] : 0;
             }
 
             $this->manageStorage->connect()->updateOne(
