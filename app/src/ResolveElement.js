536cb460f7e7a028c03ad4237bb67117f32fd9d3
Yosmany Garcia <yosmanyga@gmail.com>
2017-05-30 15:43:41 -0700

    Fixed element resolving to accept hostnames

--- app/src/ResolveElement.js
+++ app/src/ResolveElement.js
@@ -1,26 +1,45 @@
 export default class ResolveElement
 {
     resolve(url, items) {
+        /* Analyze by hostname */
+
         let item = items.find((item) => {
             const type = typeof item.hostname;
-            if (type !== 'undefined') {
-                if (type === 'string') {
-                    item.hostname = [item.hostname];
-                }
 
-                return item.hostname.filter((hostname) => {
-                    return (
-                        hostname === window.location.hostname
-                        || 'www.' + hostname === window.location.hostname
-                    );
-                })
+            if (type === 'undefined') {
+                return false;
             }
 
+            if (type === 'string') {
+                item.hostname = [item.hostname];
+            }
+
+            return item.hostname.find((hostname) => {
+                return (
+                    hostname === window.location.hostname
+                    || 'www.' + hostname === window.location.hostname
+                );
+            })
+        });
+
+        if (typeof item !== 'undefined') {
+            return item.element;
+        }
+
+        /* Analyze by url */
+
+        item = items.find((item) => {
             return (
                 typeof item.url !== 'undefined'
                 && url.startsWith(item.url)
             );
         });
+
+        if (typeof item !== 'undefined') {
+            return item.element;
+        }
+
+        /* Default */
 
         if (typeof item === 'undefined') {
             item = items.find((item) => {
@@ -31,6 +50,10 @@ export default class ResolveElement
             });
         }
 
-        return item.element;
+        if (typeof item !== 'undefined') {
+            return item.element;
+        }
+
+        throw new Error();
     }
 }
