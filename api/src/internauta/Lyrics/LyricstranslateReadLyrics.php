d42b622b67bbd29f26b1c507ca9e25fb4c3bdd22
Yosmany Garcia <yosmanyga@gmail.com>
2017-12-09 18:20:37 -0800

    Added lyricstranslate lyrics service

---
+++ api/src/internauta/Lyrics/LyricstranslateReadLyrics.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Muchacuba\Internauta\Lyrics;
+
+use Cubalider\Navigation\RequestPage;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * @di\service({
+ *     tags: [{name: 'internauta.lyrics.read_lyrics', key: 'lyricstranslate'}]
+ * })
+ */
+class LyricstranslateReadLyrics implements ReadLyrics
+{
+    /**
+     * @var RequestPage
+     */
+    private $requestPage;
+
+    /**
+     * @param RequestPage $requestPage
+     */
+    public function __construct(RequestPage $requestPage)
+    {
+        $this->requestPage = $requestPage;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function read($link)
+    {
+        if (parse_url($link, PHP_URL_HOST) !== 'lyricstranslate.com') {
+            throw new UnsupportedLinkException();
+        }
+
+        $crawler = $this->requestPage->request($link);
+
+        $author = $this->resolveAuthor($crawler);
+
+        $title = $this->resolveTitle($crawler);
+
+        $lyrics = $this->resolveLyrics($crawler);
+
+        return [$author, $title, $lyrics];
+    }
+
+    /**
+     * @param Crawler $crawler
+     *
+     * @return string
+     */
+    private function resolveAuthor(Crawler $crawler)
+    {
+        $crawler = $crawler->filter('.song-node-info-artist a');
+
+        $author = $crawler->text();
+
+        return $author;
+    }
+
+    /**
+     * @param Crawler $crawler
+     *
+     * @return string
+     */
+    private function resolveTitle(Crawler $crawler)
+    {
+        $crawler = $crawler->filter('h2.title-h2');
+
+        $title = $crawler->text();
+        $title = trim($title);
+
+        return $title;
+    }
+
+    /**
+     * @param Crawler $crawler
+     *
+     * @return string
+     */
+    private function resolveLyrics(Crawler $crawler)
+    {
+        $crawler = $crawler->filter('.ltf');
+
+        $lyrics = $crawler->text();
+
+        return $lyrics;
+    }
+}
